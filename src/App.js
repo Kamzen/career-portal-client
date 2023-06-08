77b17d02ec806f120be44773e2006d2b69454e58
Kamzen <kamzen1994@gmail.com>
2023-06-08 18:09:03 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import './App.css';
-import Navigation from './components/navigation/Navigation';
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import Home from './pages/Home';
+import "./App.css";
+import Navigation from "./components/navigation/Navigation";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import { ThemeProvider } from "@emotion/react";
+import theme from "./theme";
 
 function App() {
   return (
-
-    <Router>
-      <Routes>
-        <Route path='/' element={<Navigation />}>
-          <Route path='/' element={<Home />} />
-        </Route>
-      </Routes>
-    </Router>
-
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Navigation />}>
+            <Route path="/" element={<Home />} />
+          </Route>
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
 
