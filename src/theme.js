8bcec468bb21fb4b904852951f3de138272c2d1e
Kamzen <kamzen1994@gmail.com>
2023-06-08 22:34:37 +0200

    Update theme.js

--- src/theme.js
+++ src/theme.js
@@ -1,14 +1,93 @@
 import { createTheme } from "@mui/material";
 
-const theme = createTheme({
+export const themeDark = createTheme({
+  components: {
+    MuiPaper: {
+      styleOverrides: {
+        root: {
+          border: "1px solid #202020ff",
+          borderRadius: "10px",
+        },
+      },
+    },
+    MuiCard: {
+      styleOverrides: {
+        root: {
+          border: "1px solid #202020ff",
+          borderRadius: "5px",
+        },
+      },
+    },
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          borderRadius: 8,
+        },
+      },
+    },
+  },
   palette: {
+    mode: "dark",
     primary: {
-      main: '#163683'
+      light: "#3a9bfb",
+      main: "#1CCAFF",
+      info: "#333",
+      contrastText: "#333",
     },
     secondary: {
-      main: '#a6aa15'
-    }
-  }
-})
+      main: "#0FFFB3",
+    },
+    error: {
+      light: "#e57373",
+      main: "#f44336",
+      dark: "#d32f2f",
+      contrastText: "#333",
+    },
+    background: {
+      // paper: "#444444da",
+      paper: "#202020ff",
+      default: "#191919ff",
+    },
+  },
+});
 
-export default theme;
+export const themeLight = createTheme({
+  components: {
+    MuiPaper: {
+      styleOverrides: {
+        root: {},
+      },
+    },
+    MuiCard: {
+      styleOverrides: {
+        root: {
+          borderRadius: "10px",
+        },
+      },
+    },
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          borderRadius: 8,
+        },
+      },
+    },
+  },
+  palette: {
+    mode: "light",
+    primary: {
+      light: "#3a9bfb",
+      main: "#14367c",
+      info: "#333",
+      contrastText: "#ffffff",
+    },
+    secondary: {
+      main: "#14a37f",
+      contrastText: "#ffffff",
+    },
+    background: {
+      paper: "#fbfbfbff",
+      default: "#f3f3f3ff",
+    },
+  },
+});
