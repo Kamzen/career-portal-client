ba5a8542788232b4b9dc111a7f0867297383005c
Kamzen <kamzen1994@gmail.com>
2023-06-12 15:30:05 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // import "./App.css";
 import Navigation from "./components/navigation/Navigation";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import Home from "./pages/Home";
+import { Navigate, Route, BrowserRouter as Router, Routes } from "react-router-dom";
+// import Home from "./pages/Home";
 import { ThemeProvider } from "@emotion/react";
 import { themeDark, themeLight } from "./theme";
 import { CssBaseline } from "@mui/material";
@@ -17,7 +17,7 @@ function App() {
       <Router>
         <Navigation setThemeMode={setThemeMode} currentTheme={themeMode}>
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={<Navigate to='/login' />} />
             <Route path="/login" element={<LoginUser />} />
           </Routes>
         </Navigation>
