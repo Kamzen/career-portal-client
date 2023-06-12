fb9488dd926e885416615526590fe01f24c7d877
Kamzen <kamzen1994@gmail.com>
2023-06-12 10:38:30 +0200

    Update ProfessionalsSkills.jsx

--- src/components/ProfessionalsSkills.jsx
+++ src/components/ProfessionalsSkills.jsx
@@ -1,9 +1,149 @@
-import React from 'react'
+import {
+  Box,
+  Button,
+  Dialog,
+  DialogContent,
+  Grid,
+  IconButton,
+  InputLabel,
+  Paper,
+  Stack,
+  Typography,
+  useMediaQuery
+} from "@mui/material";
+import React from "react";
+
+import CloseIcon from "@mui/icons-material/Close";
+import { useTheme } from "@mui/material/styles";
+import { Form, Formik } from "formik";
+import TextFieldWrapper from "./form-components/TextFieldWrapper";
+import SelectFieldWrapper from "./form-components/SelectFieldWrapper";
 
 const ProfessionalsSkills = () => {
   return (
-    <div>ProfessionalsSkills</div>
-  )
-}
+    <Stack
+      minHeight={100}
+      padding={2}
+      sx={{ position: "relative" }}
+      component={Paper}
+    >
+      <Stack
+        // border={1}
+        width="100%"
+        direction="row"
+        justifyContent="end"
+        sx={{ position: "absolute", bottom: 0, left: 0, padding: 2 }}
+      >
+        <ProfessionalsSkillsModal />
+      </Stack>
+    </Stack>
+  );
+};
 
-export default ProfessionalsSkills
+const ProfessionalsSkillsModal = () => {
+  const [open, setOpen] = React.useState(false);
+  const theme = useTheme();
+  const fullScreen = useMediaQuery(theme.breakpoints.down("md"));
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <Button
+        variant="contained"
+        sx={{ fontSize: 12 }}
+        onClick={handleClickOpen}
+      >
+        Add Professional Skills
+      </Button>
+      <Dialog fullScreen={fullScreen} open={open} onClose={handleClose}>
+        <Stack
+          direction="row"
+          justifyContent="space-between"
+          alignItems="center"
+          padding={2}
+          sx={{
+            backgroundColor: "primary.main",
+            height: 40,
+            color: "#FFFFFF",
+            fontWeight: "bolder"
+          }}
+        >
+          <Typography>Add Professional Skills</Typography>
+          <IconButton onClick={handleClose}>
+            <CloseIcon sx={{ color: "#FFFFFF" }} />
+          </IconButton>
+        </Stack>
+        <DialogContent>
+          <Formik
+            initialValues={{
+              skill: "",
+              skillLevel: ""
+            }}
+          >
+            {(formik) => {
+              return (
+                <Form>
+                  <Grid container spacing={2}>
+                    <Grid item xs={12} md={6}>
+                      <InputLabel sx={{ mb: 1 }}>Skill</InputLabel>
+                      <TextFieldWrapper name="skill" label="Skill" />
+                    </Grid>
+                    <Grid item xs={12} md={6}>
+                      <InputLabel sx={{ mb: 1 }}>Skill Level</InputLabel>
+                      <SelectFieldWrapper
+                        name="skillLevel"
+                        label="Skill Level"
+                        options={[
+                          {
+                            value: "Beginner",
+                            label: "Beginner"
+                          },
+                          {
+                            value: "Intermediate",
+                            label: "Intermediate"
+                          },
+                          {
+                            value: "Expert",
+                            label: "Expert"
+                          }
+                        ]}
+                      />
+                    </Grid>
+                    <Grid item xs={12} md={12}>
+                      <Box textAlign="end">
+                        <Button
+                          variant="outlined"
+                          autoFocus
+                          onClick={handleClose}
+                        >
+                          Close
+                        </Button>
+                        <Button
+                          variant="contained"
+                          onClick={handleClose}
+                          autoFocus
+                          sx={{ ml: 2, px: 3 }}
+                        >
+                          Add
+                        </Button>
+                      </Box>
+                    </Grid>
+                  </Grid>
+                </Form>
+              );
+            }}
+          </Formik>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+};
+
+export default ProfessionalsSkills;
