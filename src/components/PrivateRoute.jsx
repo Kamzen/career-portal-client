8a2fe64ccf4bc4e40b054b0ebfc4c80f31db7d1b
Kamzen <kamzen1994@gmail.com>
2023-06-14 16:53:13 +0200

    Create PrivateRoute.jsx

---
+++ src/components/PrivateRoute.jsx
@@ -0,0 +1,63 @@
+import { useQuery } from "@tanstack/react-query";
+import React from "react";
+import ApiQueries from "../apiQuries";
+import { Alert, LinearProgress, Snackbar } from "@mui/material";
+import { Navigate, Outlet } from "react-router-dom";
+
+const PrivateRoute = () => {
+  const [open, setOpen] = React.useState(true);
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  const { data, isSuccess, isLoading, error } = useQuery({
+    queryKey: ["userInfo"],
+    queryFn: () => {
+      return ApiQueries.userInfo();
+    },
+
+    // staleTime: 1000 * 60 * 60 * 24
+  });
+
+  if (isLoading) {
+    return <LinearProgress />;
+  }
+
+  if (error?.code === "ERR_NETWORK") {
+    return (
+      <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="error" sx={{ width: "100%" }}>
+          You are offline
+        </Alert>
+      </Snackbar>
+    );
+  }
+
+  if (error?.response.status === 500) {
+    return (
+      <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="error" sx={{ width: "100%" }}>
+          Technical issue, please note that fasset IT team is busy fixing the
+          problem.
+        </Alert>
+      </Snackbar>
+    );
+  }
+
+  if (error?.response.status === 401) {
+    return <Navigate to="/login" />;
+  }
+
+  return isSuccess && data ? (
+    <Outlet />
+  ) : (
+    <Navigate to="/login" />
+  );
+};
+
+export default PrivateRoute;
