9d59038da4ceb9d244bcaaea3db258c5f8c0767f
Kamzen <kamzen1994@gmail.com>
2023-06-14 16:53:11 +0200

    Update LoginUser.jsx

--- src/pages/auth/LoginUser.jsx
+++ src/pages/auth/LoginUser.jsx
@@ -1,8 +1,10 @@
 import {
+  Alert,
   Button,
   Card,
   Grid,
   InputLabel,
+  LinearProgress,
   Link,
   Stack,
   Typography
@@ -13,9 +15,35 @@ import React from "react";
 import TextFieldWrapper from "../../components/form-components/TextFieldWrapper";
 import logo from "../../images/white_logo.png";
 import { useNavigate } from "react-router-dom";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import ApiQueries from "../../apiQuries";
+import AlertPopup from "../../components/AlertPopup";
 
 const LoginUser = () => {
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
+
+  const { mutate, error, isSuccess, isLoading, data } = useMutation({
+    mutationFn: (formData) => {
+      const data = ApiQueries.loginUser(formData);
+
+      return data;
+    },
+
+    onSuccess: (data) => {
+      localStorage.setItem('user', JSON.stringify(data.user));
+      queryClient.invalidateQueries({
+        queryKey: 'userInfo',
+      })
+      setTimeout(() => {
+        navigate('/')
+      }, 3000)
+    },
+    onError: (err) => {
+      console.log(err);
+    }
+  });
+
   return (
     <Stack
       width="100%"
@@ -23,6 +51,8 @@ const LoginUser = () => {
       // height={{ xs: "80vh" }}
       justifyContent="center"
     >
+      {isSuccess && <AlertPopup open={true} message={data.message} />}
+
       <Grid container>
         <Grid item xs={12} md={12}>
           <Formik
@@ -38,13 +68,16 @@ const LoginUser = () => {
                 .required("Password required")
                 .min(8, "At least 8 characters required for password")
             })}
+            onSubmit={(values) => {
+              mutate(values);
+            }}
           >
             {() => {
               return (
                 <Form>
                   <Card sx={{ width: { md: "40%", xs: "100%" }, m: "auto" }}>
                     <Stack
-                      border={1}
+                      // border={1}
                       height={130}
                       alignItems="center"
                       justifyContent="center"
@@ -53,6 +86,8 @@ const LoginUser = () => {
                     >
                       <img src={logo} alt="" width={100} height={100} />
                     </Stack>
+
+                    {isLoading && <LinearProgress />}
 
                     <Typography
                       component={Stack}
@@ -66,8 +101,15 @@ const LoginUser = () => {
                     >
                       Login
                     </Typography>
-                    <Grid container padding={2} rowGap={2}>
-                      <Grid xs={12} md={12}>
+
+                    {error?.response?.status === 404 && (
+                      <Alert severity="error" color="error" sx={{ m: 2 }}>
+                        {error?.response?.data?.message}
+                      </Alert>
+                    )}
+
+                    <Grid container padding={2} spacing={2}>
+                      <Grid item xs={12} md={12}>
                         <InputLabel>Email</InputLabel>
                         <TextFieldWrapper
                           name="email"
@@ -75,7 +117,7 @@ const LoginUser = () => {
                           sx={{ mt: 2 }}
                         />
                       </Grid>
-                      <Grid xs={12} md={12}>
+                      <Grid item xs={12} md={12}>
                         <Stack
                           direction="row"
                           justifyContent="space-between"
@@ -93,19 +135,19 @@ const LoginUser = () => {
                           sx={{ mt: 2 }}
                         />
                       </Grid>
-                      <Grid xs={12} md={12}>
+                      <Grid item xs={12} md={12}>
                         <Button type="submit" variant="contained" fullWidth>
                           Login
                         </Button>
                       </Grid>
-                      <Grid xs={12} md={12}>
+                      <Grid item xs={12} md={12}>
                         <Typography>
-                          Don't Have An Account?{" "}
+                          Don't Have An Account?
                           <Link
                             sx={{ textDecoration: "none" }}
                             onClick={() => navigate("/register")}
                           >
-                            Rgister
+                            Register
                           </Link>
                         </Typography>
                       </Grid>
