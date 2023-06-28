0b3b1408a38923dd6730ac6deeeb58a03598d977
Kamzen <kamzen1994@gmail.com>
2023-06-28 12:58:22 +0200

    Update EditLearnerBasicInformation.jsx

--- src/components/modals/EditLearnerBasicInformation.jsx
+++ src/components/modals/EditLearnerBasicInformation.jsx
@@ -5,25 +5,25 @@ import DialogContent from "@mui/material/DialogContent";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import { useTheme } from "@mui/material/styles";
 import {
-  Autocomplete,
   Box,
-  Checkbox,
   Grid,
   IconButton,
   InputLabel,
+  LinearProgress,
   Stack,
-  TextField,
   Typography
 } from "@mui/material";
 import { Form, Formik } from "formik";
 import TextFieldWrapper from "../form-components/TextFieldWrapper";
 import SelectFieldWrapper from "../form-components/SelectFieldWrapper";
 import CloseIcon from "@mui/icons-material/Close";
-import CheckBoxOutlineBlankIcon from "@mui/icons-material/CheckBoxOutlineBlank";
-import CheckBoxIcon from "@mui/icons-material/CheckBox";
-import * as Yup from "yup";
 
-export default function EditLearnerBasicInformation({userInfo}) {
+import * as Yup from "yup";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import ApiQueries from "../../apiQuries";
+import AlertPopup from "../AlertPopup";
+
+export default function EditLearnerBasicInformation({ userInfo }) {
   const [open, setOpen] = React.useState(false);
   const theme = useTheme();
   const fullScreen = useMediaQuery(theme.breakpoints.down("md"));
@@ -36,8 +36,19 @@ export default function EditLearnerBasicInformation({userInfo}) {
     setOpen(false);
   };
 
-  const icon = <CheckBoxOutlineBlankIcon fontSize="small" />;
-  const checkedIcon = <CheckBoxIcon fontSize="small" />;
+  const queryClient = useQueryClient();
+
+  const { mutate, isSuccess, error, isLoading, data, isError } = useMutation({
+    mutationFn: (formData) => {
+      return ApiQueries.editBasicInformation(formData)
+    },
+    onSuccess: (data) => {
+      queryClient.invalidateQueries(['userInfo'])
+    },
+    onError: (err) => {
+      console.log(err)
+    }
+  })
 
   const careerStatusOptions = [
     {
@@ -66,6 +77,25 @@ export default function EditLearnerBasicInformation({userInfo}) {
     }
   ];
 
+  const raceOptions = [
+    {
+      value: "Black",
+      label: "Black"
+    }, 
+    {
+      value: "White",
+      label: "White"
+    }, 
+    {
+      value: "Coloured",
+      label: "Coloured"
+    }, 
+    {
+      value: "Indian",
+      label: "Indian"
+    }
+  ]
+
   return (
     <div>
       <Button
@@ -75,6 +105,15 @@ export default function EditLearnerBasicInformation({userInfo}) {
       >
         Edit Basic Information
       </Button>
+      {
+        error && <AlertPopup open={true} message={error.response.data.message} severity='error' />
+      }
+      {
+        isSuccess && !isError && data && <AlertPopup open={true} message={data.message} />
+      }
+      {
+        isError && <AlertPopup open={true} message='Server Error' severity='error' />
+      }
       <Dialog fullScreen={fullScreen} open={open} onClose={handleClose}>
         <Stack
           direction="row"
@@ -89,6 +128,9 @@ export default function EditLearnerBasicInformation({userInfo}) {
           }}
         >
           <Typography>Edit Basic Information</Typography>
+          {
+            isLoading && <LinearProgress />
+          }
           <IconButton onClick={handleClose}>
             <CloseIcon sx={{ color: "#FFFFFF" }} />
           </IconButton>
@@ -96,12 +138,14 @@ export default function EditLearnerBasicInformation({userInfo}) {
         <DialogContent>
           <Formik
             initialValues={{
+              id: userInfo?.id || "",
               firstName: userInfo?.firstName || "",
               middleName: userInfo?.middleName || "",
               lastName: userInfo?.lastName || "",
-              identificationNumber: userInfo?.studentInformation?.identificationNumber || "",
-              disability: userInfo?.studentInformation?.disability || "",
-              careerStatus: userInfo?.studentInformation?.careerStatus ||  [],
+              identificationNumber:
+                userInfo?.studentInformation?.identificationNumber || "",
+              disbility: userInfo?.studentInformation?.disbility || "",
+              careerStatus: userInfo?.studentInformation?.careerStatus || "",
               mobileNumber: userInfo?.studentInformation?.mobileNumber || "",
               race: userInfo?.studentInformation?.race || ""
             }}
@@ -110,9 +154,11 @@ export default function EditLearnerBasicInformation({userInfo}) {
               lastName: Yup.string().required("LastName required"),
               careerStatus: Yup.string().required("Career status required"),
               mobileNumber: Yup.string()
-                .required("Mobile number required")
-                .min(10, "At least 10 digits, eg 0797126016"),
+                .required("Mobile number required"),
               race: Yup.string().required("Race is required"),
+              disbility: Yup.string().required(
+                "Disability status is required"
+              ),
               identificationNumber: Yup.string()
                 .required("ID Number is a required field")
                 .test(
@@ -159,8 +205,12 @@ export default function EditLearnerBasicInformation({userInfo}) {
                   }
                 )
             })}
+
+            onSubmit={(values) => {
+              mutate(values)
+            }}
           >
-            {(formik) => {
+            {({errors}) => {
               return (
                 <Form>
                   <Grid container spacing={2}>
@@ -191,7 +241,7 @@ export default function EditLearnerBasicInformation({userInfo}) {
                     <Grid item xs={12} md={6}>
                       <InputLabel>Identification Number</InputLabel>
                       <TextFieldWrapper
-                        name="id"
+                        name="identificationNumber"
                         label="Identification Number"
                         sx={{ mt: 1 }}
                       />
@@ -199,7 +249,7 @@ export default function EditLearnerBasicInformation({userInfo}) {
                     <Grid item xs={12} md={6}>
                       <InputLabel sx={{ mb: 1 }}>Disability Status</InputLabel>
                       <SelectFieldWrapper
-                        name="disability"
+                        name="disbility"
                         label="Disability Status"
                         options={[
                           {
@@ -215,37 +265,21 @@ export default function EditLearnerBasicInformation({userInfo}) {
                     </Grid>
                     <Grid item xs={12} md={6}>
                       <InputLabel sx={{ mb: 1 }}>Career Status</InputLabel>
-                      <Autocomplete
-                        fullWidth
-                        multiple
-                        size="small"
+                      <SelectFieldWrapper
                         name="careerStatus"
+                        label="Career Status"
                         options={careerStatusOptions}
-                        disableCloseOnSelect
-                        getOptionLabel={(option) => option.label}
-                        renderOption={(props, option, { selected }) => (
-                          <li {...props}>
-                            <Checkbox
-                              icon={icon}
-                              checkedIcon={checkedIcon}
-                              style={{ marginRight: 8 }}
-                              checked={selected}
-                            />
-                            {option.label}
-                          </li>
-                        )}
-                        // style={{ width: 500 }}
-                        renderInput={(params) => (
-                          <TextField
-                            {...params}
-                            label="Career Status"
-                            fullWidth
-                            size="small"
-                          />
-                        )}
                       />
                     </Grid>
-                    <Grid item xs={12} md={12}>
+                    <Grid item xs={12} md={6}>
+                      <InputLabel sx={{ mb: 1 }}>Race</InputLabel>
+                      <SelectFieldWrapper
+                        name="race"
+                        label="Race"
+                        options={raceOptions}
+                      />
+                    </Grid>
+                    <Grid item xs={12} md={16}>
                       <InputLabel>Mobile Number</InputLabel>
                       <TextFieldWrapper
                         name="mobileNumber"
