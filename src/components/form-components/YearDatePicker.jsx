cc268a90491adc68b571e76003da085cfa0d19c1
Kamzen <kamzen1994@gmail.com>
2023-08-30 10:23:13 +0200

    Create YearDatePicker.jsx

---
+++ src/components/form-components/YearDatePicker.jsx
@@ -0,0 +1,45 @@
+import { FormControl, TextField } from "@mui/material";
+import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { useField, useFormikContext } from "formik";
+import React from "react";
+
+const YearDatePicker = ({ name, label, ...otherProps }) => {
+  const [field, meta] = useField(name);
+
+  const { setFieldValue } = useFormikContext();
+
+  const configField = {
+    ...field,
+    ...otherProps,
+    fullWidth: true,
+    variant: "outlined"
+  };
+
+  const configError = {};
+
+  if (meta && meta.touched && meta.error) {
+    configError.error = true;
+    configError.helperText = meta.error;
+  }
+
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <FormControl fullWidth size="small">
+        <DatePicker
+          views={["year"]}
+          label={label}
+          {...configField}
+          onChange={(date) => {
+            setFieldValue(name, new Date(date).getFullYear().toString());
+          }}
+          renderInput={(params) => (
+            <TextField {...params} {...configError} size="small" />
+          )}
+        />
+      </FormControl>
+    </LocalizationProvider>
+  );
+};
+
+export default YearDatePicker;
