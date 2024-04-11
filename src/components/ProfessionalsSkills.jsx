18c3e551cf75c0e3deec41e07d78d6ad839757a9
Kamzen <37660966+Kamzen@users.noreply.github.com>
2024-04-11 19:31:11 +0200

    Update ProfessionalsSkills.jsx

--- src/components/ProfessionalsSkills.jsx
+++ src/components/ProfessionalsSkills.jsx
@@ -1,8 +1,23 @@
-import { Paper, Stack } from "@mui/material";
+import { LinearProgress, Paper, Stack } from "@mui/material";
 import React from "react";
 import ProfessionalsSkillsModal from "./modals/ProfessionalSkillsModal";
+import { useQuery } from "@tanstack/react-query";
+import ApiQueries from "../apiQuries";
 
 const ProfessionalsSkills = () => {
+  const { data, isLoading } = useQuery({
+    queryKey: ["userInfo"],
+    queryFn: () => {
+      return ApiQueries.userInfo();
+    },
+  });
+
+  console.log(data);
+
+  if (isLoading) {
+    return <LinearProgress />;
+  }
+
   return (
     <Stack
       minHeight={100}
@@ -17,7 +32,7 @@ const ProfessionalsSkills = () => {
         justifyContent="end"
         sx={{ position: "absolute", bottom: 0, left: 0, padding: 2 }}
       >
-        <ProfessionalsSkillsModal />
+        <ProfessionalsSkillsModal userId={data?.id} />
       </Stack>
     </Stack>
   );
