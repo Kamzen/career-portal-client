b0de97755d7b7cb5c7c3048dbb0c938efda0cf99
Kamzen <37660966+Kamzen@users.noreply.github.com>
2024-04-18 11:24:16 +0200

    Update ProfessionalsSkills.jsx

--- src/components/ProfessionalsSkills.jsx
+++ src/components/ProfessionalsSkills.jsx
@@ -1,15 +1,28 @@
-import { LinearProgress, Paper, Stack } from "@mui/material";
+import {
+  Alert,
+  LinearProgress,
+  Paper,
+  Stack,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography
+} from "@mui/material";
 import React from "react";
 import ProfessionalsSkillsModal from "./modals/ProfessionalSkillsModal";
 import { useQuery } from "@tanstack/react-query";
 import ApiQueries from "../apiQuries";
+import { DeleteSkillModal } from "./modals/DeleteSkillModal";
 
 const ProfessionalsSkills = () => {
   const { data, isLoading } = useQuery({
     queryKey: ["userInfo"],
     queryFn: () => {
       return ApiQueries.userInfo();
-    },
+    }
   });
 
   console.log(data);
@@ -20,20 +33,99 @@ const ProfessionalsSkills = () => {
 
   return (
     <Stack
-      minHeight={100}
+      height={518}
       padding={2}
-      sx={{ position: "relative" }}
+      spacing={2}
       component={Paper}
+      sx={{ overflowY: "auto" }}
     >
       <Stack
         // border={1}
         width="100%"
         direction="row"
-        justifyContent="end"
-        sx={{ position: "absolute", bottom: 0, left: 0, padding: 2 }}
+        justifyContent="space-between"
       >
+        <Typography
+          sx={{ fontSize: 20, textAlign: "center", fontWeight: "bolder" }}
+        >
+          Professional Skills
+        </Typography>
         <ProfessionalsSkillsModal userId={data?.id} />
       </Stack>
+
+      {data?.skills?.length > 0 ? (
+        <TableContainer component={Paper}>
+          <Table aria-label="simple table">
+            <TableHead sx={{ backgroundColor: "background.paper" }}>
+              <TableRow>
+                <TableCell align="center" sx={{ fontWeight: "bolder" }}>
+                  No#
+                </TableCell>
+                <TableCell align="center" sx={{ fontWeight: "bolder" }}>
+                  Skill
+                </TableCell>
+
+                <TableCell align="center" sx={{ fontWeight: "bolder" }}>
+                  Skill Level
+                </TableCell>
+
+                <TableCell align="center" sx={{ fontWeight: "bolder" }}>
+                  Action
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {data?.skills?.map((skill, i) => {
+                return (
+                  <TableRow key={skill.id}>
+                    <TableCell align="center" component="th" scope="row">
+                      {i + 1}
+                    </TableCell>
+                    <TableCell align="center" component="th" scope="row">
+                      {skill.skill}
+                    </TableCell>
+                    <TableCell align="center" component="th" scope="row">
+                      {skill.skillLevel}
+                    </TableCell>
+                    <TableCell align="center">
+                      <Stack
+                        direction="row"
+                        spacing={2}
+                        justifyContent="center"
+                      >
+                        <ProfessionalsSkillsModal skill={skill} />
+                        <DeleteSkillModal id={skill.id} />
+                      </Stack>
+                    </TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+            {/* <TableFooter>
+            <TableRow>
+              <TablePagination
+                rowsPerPageOptions={[5, 10, 25, { label: "All", value: -1 }]}
+                // colSpan={3}
+                count={employees?.length || 0}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                SelectProps={{
+                  inputProps: {
+                    "aria-label": "rows per page",
+                  },
+                  native: true,
+                }}
+                onPageChange={handleChangePage}
+                onRowsPerPageChange={handleChangeRowsPerPage}
+                ActionsComponent={TablePaginationActions}
+              />
+            </TableRow>
+          </TableFooter> */}
+          </Table>
+        </TableContainer>
+      ) : (
+        <Alert severity="info">Professional skills are optional</Alert>
+      )}
     </Stack>
   );
 };
