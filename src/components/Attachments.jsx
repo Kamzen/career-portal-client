1742e0dff2e6a84c4dc671a564e9b8de96836f55
Kamzen <37660966+Kamzen@users.noreply.github.com>
2024-04-19 22:52:49 +0200

    Update Attachments.jsx

--- src/components/Attachments.jsx
+++ src/components/Attachments.jsx
@@ -1,177 +1,30 @@
-import {
-  Box,
-  Button,
-  Dialog,
-  DialogContent,
-  Grid,
-  IconButton,
-  InputLabel,
-  Paper,
-  Stack,
-  TextField,
-  Typography,
-  useMediaQuery
-} from "@mui/material";
+import { Paper, Stack, Typography } from "@mui/material";
 import React from "react";
-
-import CloseIcon from "@mui/icons-material/Close";
-import { useTheme } from "@mui/material/styles";
-import { Field, Form, Formik } from "formik";
-import SelectFieldWrapper from "./form-components/SelectFieldWrapper";
+import AddEditAttachmentModal from "./modals/AddEditAttachmentModal";
 
 const Attachments = () => {
   return (
     <Stack
-      minHeight={100}
+      height={518}
       padding={2}
-      sx={{ position: "relative" }}
+      spacing={2}
       component={Paper}
+      sx={{ overflowY: "auto" }}
     >
       <Stack
         // border={1}
         width="100%"
         direction="row"
-        justifyContent="end"
-        sx={{ position: "absolute", bottom: 0, left: 0, padding: 2 }}
+        justifyContent="space-between"
       >
-        <AttachmentsModal />
+        <Typography
+          sx={{ fontSize: 20, textAlign: "center", fontWeight: "bolder" }}
+        >
+          Documents
+        </Typography>
+        <AddEditAttachmentModal />
       </Stack>
     </Stack>
-  );
-};
-
-const AttachmentsModal = () => {
-  const [open, setOpen] = React.useState(false);
-  const theme = useTheme();
-  const fullScreen = useMediaQuery(theme.breakpoints.down("md"));
-
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
-
-  return (
-    <div>
-      <Button
-        variant="contained"
-        sx={{ fontSize: 12 }}
-        onClick={handleClickOpen}
-      >
-        Add Attachment
-      </Button>
-      <Dialog fullScreen={fullScreen} open={open} onClose={handleClose}>
-        <Stack
-          direction="row"
-          justifyContent="space-between"
-          alignItems="center"
-          padding={2}
-          sx={{
-            backgroundColor: "primary.main",
-            height: 40,
-            color: "#FFFFFF",
-            fontWeight: "bolder"
-          }}
-        >
-          <Typography>Add Attachment</Typography>
-          <IconButton onClick={handleClose}>
-            <CloseIcon sx={{ color: "#FFFFFF" }} />
-          </IconButton>
-        </Stack>
-        <DialogContent>
-          <Formik
-            initialValues={{
-              documentName: "",
-              file: ""
-            }}
-          >
-            {(formik) => {
-              return (
-                <Form>
-                  <Grid container spacing={2}>
-                    <Grid item xs={12} md={6}>
-                      <InputLabel sx={{ mb: 1 }}>Document Name</InputLabel>
-                      <SelectFieldWrapper
-                        name="course"
-                        label="Document Name"
-                        options={[
-                          {
-                            value: "Matric Certificate",
-                            label: "Matric Certificate"
-                          },
-                          {
-                            value: "Qualification",
-                            label: "Qualification"
-                          },
-                          {
-                            value: "Id Document",
-                            label: "Id Document"
-                          },
-                          {
-                            value: "Certificate & Training",
-                            label: "Certificate & Training"
-                          }
-                        ]}
-                      />
-                    </Grid>
-                    <Grid item xs={12} md={6}>
-                      <InputLabel sx={{ mb: 1 }}>File</InputLabel>
-                      <Field name="file">
-                        {({ field, form, meta }) => (
-                          <TextField
-                            type="file"
-                            label="File"
-                            InputLabelProps={{
-                              shrink: true
-                            }}
-                            // inputProps={{
-                            //   accept:
-                            //     ".doc, .docx, application/msword, application/vnd.openxmlformats-officedocument.wordprocessingml.document"
-                            // }}
-                            error={meta.touched && meta.error}
-                            helperText={
-                              meta.touched && meta.error && meta.error
-                            }
-                            fullWidth
-                            onChange={(event) => {
-                              form.setFieldValue(
-                                field.name,
-                                event.currentTarget.files[0]
-                              );
-                            }}
-                          />
-                        )}
-                      </Field>
-                    </Grid>
-                    <Grid item xs={12} md={12}>
-                      <Box textAlign="end">
-                        <Button
-                          variant="outlined"
-                          autoFocus
-                          onClick={handleClose}
-                        >
-                          Close
-                        </Button>
-                        <Button
-                          variant="contained"
-                          onClick={handleClose}
-                          autoFocus
-                          sx={{ ml: 2, px: 3 }}
-                        >
-                          Upload
-                        </Button>
-                      </Box>
-                    </Grid>
-                  </Grid>
-                </Form>
-              );
-            }}
-          </Formik>
-        </DialogContent>
-      </Dialog>
-    </div>
   );
 };
 
